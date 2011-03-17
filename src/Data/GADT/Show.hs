3719026b60977aeed9118bb1557f944d6b26063a
James Cook <mokus@deepbondi.net>
2011-03-17 11:43:17 -0400

    Added a higher-kinded 'Read'-like class

--- src/Data/GADT/Show.hs
+++ src/Data/GADT/Show.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes, ImpredicativeTypes #-}
 module Data.GADT.Show where
 
 -- |'Show'-like class for 1-type-parameter GADTs; I'm not entirely sure
@@ -15,3 +16,17 @@ gshows = gshowsPrec showsPrec (-1)
 
 gshow :: (GShow t, Show a) => t a -> String
 gshow x = gshows x ""
+
+
+type GReadS t = String -> [(forall b. (forall a. t a -> b) -> b, String)]
+class GRead t where
+    greadsPrec :: Int -> GReadS t
+
+greads :: GRead t => GReadS t
+greads = greadsPrec (-1)
+
+gread :: GRead t => String -> (forall a. t a -> b) -> b
+gread s = hd [f | (f, "") <- greads s]
+    where
+        hd (x:_) = x
+        hd _ = error "gread: No parse"
