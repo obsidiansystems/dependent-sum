f3b50470f572d6722a30eb6ed509c32c20f4b4c6
Ken Micklas <kmicklas@gmail.com>
2019-03-25 11:10:05 -0400

    Deprecate This constructor name in favor of Some

--- src/Data/Some.hs
+++ src/Data/Some.hs
@@ -5,7 +5,7 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE ViewPatterns #-}
-module Data.Some (Some(This), withSome) where
+module Data.Some (Some(Some, This), withSome) where
 
 import Data.GADT.Show
 import Data.GADT.Compare
@@ -16,31 +16,38 @@ import Unsafe.Coerce
 newtype Some tag = UnsafeSome (tag Any)
 
 #if __GLASGOW_HASKELL__ >= 801
+{-# COMPLETE Some #-}
+#endif
+pattern Some :: tag a -> Some tag
+pattern Some x <- UnsafeSome ((unsafeCoerce :: tag Any -> tag a) -> x)
+  where Some x = UnsafeSome ((unsafeCoerce :: tag a -> tag Any) x)
+
+#if __GLASGOW_HASKELL__ >= 801
 {-# COMPLETE This #-}
 #endif
+{-# DEPRECATED This "Use 'Some' instead" #-}
 pattern This :: tag a -> Some tag
-pattern This x <- UnsafeSome ((unsafeCoerce :: tag Any -> tag a) -> x)
-  where This x = UnsafeSome ((unsafeCoerce :: tag a -> tag Any) x)
+pattern This x = Some x
 
 withSome :: Some tag -> (forall a. tag a -> b) -> b
-withSome (This thing) some = some thing
+withSome (Some thing) some = some thing
 
 instance GShow tag => Show (Some tag) where
-    showsPrec p (This thing) = showParen (p > 10)
-        ( showString "This "
+    showsPrec p (Some thing) = showParen (p > 10)
+        ( showString "Some "
         . gshowsPrec 11 thing
         )
 
 instance GRead f => Read (Some f) where
     readsPrec p = readParen (p>10) $ \s ->
-        [ (getGReadResult withTag This, rest')
+        [ (getGReadResult withTag Some, rest')
         | let (con, rest) = splitAt 5 s
-        , con == "This "
+        , con == "Some "
         , (withTag, rest') <- greadsPrec 11 rest
         ]
 
 instance GEq tag => Eq (Some tag) where
-    This x == This y = defaultEq x y
+    Some x == Some y = defaultEq x y
 
 instance GCompare tag => Ord (Some tag) where
-    compare (This x) (This y) = defaultCompare x y
+    compare (Some x) (Some y) = defaultCompare x y
